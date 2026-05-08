c9432ecc381f9455fca747a5abec9959be607b51
KarthikPulusu <kpulusu0@gmail.com>
2026-05-08 12:00:27 +0530

    front end files created

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./pages/Header";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SearchHotels from "./pages/SearchHotels";
+import HotelDetails from "./pages/HotelDetails";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <></>
-  )
+    return (
+
+        <>
+
+            <Header />
+
+            <Routes>
+
+                <Route
+                    path="/"
+                    element={<Home />}
+                />
+
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+
+                <Route
+                    path="/search"
+                    element={<SearchHotels />}
+                />
+
+                <Route
+                    path="/hotel/:id"
+                    element={<HotelDetails />}
+                />
+
+                <Route
+                    path="/my-bookings"
+                    element={<MyBookings />}
+                />
+
+            </Routes>
+
+        </>
+    );
 }
 
-export default App
+export default App;
